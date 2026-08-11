d7b92916eb660b1ff3f35e9a490a581b7e9954b5
Luxinema <muhamma9amin1997@gmail.com>
2026-08-11 09:23:15 +0700

    Update pengaturan.js

--- pengaturan.js
+++ pengaturan.js
@@ -5,8 +5,7 @@
 
 const CONFIG_APP = {
     // 1. Nama Studio Foto Anda (Akan tampil di header dan portal klien)
-    namaVendor: "LUXINEMA PICTURES
-        ",
+    namaVendor: "LUXINEMA PICTURES",
 
     // 2. Google Drive API Key (Penting agar galeri bisa memuat foto)
     googleApiKey: "",AIzaSyA2b-GCOEXALZsYQJ_yHy7j4j3kGC0ROvA    whatsappAdmin: "6281379272103" // <-- TAMBAHKAN BARIS INI (Ganti dengan nomor WA admin)
